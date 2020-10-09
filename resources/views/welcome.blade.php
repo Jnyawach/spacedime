fb023ee61d19f1639301dbf55cb947c3df1b5684
jnyawach <nyawach41@gmail.com>
2020-10-09 08:48:16 +0300

    vision

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
           <div class="col-md-10 col-lg-7  d-flex align-items-end">
           	<div class="text">
 	            <h1 class="mb-4">Passionate<span>About</span>  Built Environment</h1>
-	            <p style="font-size: 18px;">Our Main vision is to help be the change you want to see</p>
+	            <p style="font-size: 18px;">To visualize the change you want to see</p>
 	            <div class="d-flex meta">
                 <div class=""><p class="mb-0"><a href="{{route('portfolio')}}" class="btn btn-primary py-3 px-2 px-md-4" title="Portfolio">Explore Projects</a></p></div>
 
